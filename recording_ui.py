152103df3f76a4b1c60b079e0e27c83fc99a4691
eve-contract-person <tom.reclik@gmail.com>
2022-03-30 15:59:07 +0200

    Initial commit

---
+++ recording_ui.py
@@ -0,0 +1,56 @@
+import tkinter as tk
+import uuid
+
+def start_recording():
+	pass
+
+class Model:
+	recordings = []
+
+class Controller:
+	def __init__(self, model, view) -> None:
+		self.model = model
+		self.view = view
+
+	def start(self):
+		self.view.setup(self)
+		self.view.start_main_loop()
+
+	def start_recording(self):
+		self.model.recordings.append(uuid.uuid4())
+		self.view.append_to_list(self.model.recordings[-1])
+	
+	def end_recording(self):
+		self.view.clear_list()
+		self.model.recordings = []
+
+class TkView:
+	def setup(self, controller):
+		self.root = tk.Tk()
+		self.root.geometry("400x400")
+		self.root.title("Voice Message Transcriber")
+
+		self.frame = tk.Frame(self.root)
+		self.frame.pack(fill=tk.BOTH, expand=1)
+		self.label = tk.Label(self.frame, text="Recording")
+		self.label.pack()
+		self.list = tk.Listbox(self.frame)
+		self.list.pack(fill=tk.BOTH, expand=1)
+		self.start_recording_button = tk.Button(self.frame, text="Start Recording", command=controller.start_recording)
+		self.start_recording_button.pack()
+		self.end_recording_button= tk.Button(self.frame, text="Start Recording", command=controller.end_recording)
+		self.end_recording_button.pack()
+
+	def append_to_list(self, item):
+		self.list.insert(tk.END, item)
+
+	def clear_list(self):
+		self.list.delete(0, tk.END)
+
+	def start_main_loop(self):
+		self.root.mainloop()
+
+
+if __name__=="__main__":
+	c = Controller(Model(), TkView())
+	c.start()
---
+++ user_story.md
@@ -0,0 +1,21 @@
+Interface
+- When the interface opens the user should see all previous recordings
+- The user should have a button to start a new recording
+- The user should have a button to end the recording 
+- The user should have a button to delete a recording
+- The user should have a button to play a recording
+
+Start Recording
+- When the user clicks the start recording button the interface should start recording
+- A preview of the recording is shown in the list of recordings
+
+End Recording
+- When the user clicks the end recording button the interface should stop recording
+- The finished recording is added to the list of recordings
+
+Delete Recording
+- When the user clicks the delete recording button the interface should delete the recording
+- The recording is removed from the list of recordings
+
+Play Recording
+- When the user clicks the play recording button the interface should play the recording
