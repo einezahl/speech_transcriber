ef5216e6543ececf0ea7a8b2a7b0293d974e79de
eve-contract-person <tom.reclik@gmail.com>
2022-03-30 22:42:11 +0200

    Added wave preview playground project

---
+++ playground/waveform_preview.py
@@ -0,0 +1,16 @@
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+import numpy as np
+
+samplerate, data = wavfile.read('./recordings/output.wav')
+
+pool_size = 500
+data = data[:data.shape[0]//pool_size*pool_size]
+data = data.reshape((-1, pool_size))
+data_mean = np.mean(data, axis=1)
+data_max = np.max(data, axis=1)
+
+fig, (ax1, ax2) = plt.subplots(2, 1)
+ax1.plot(data_mean)
+ax2.plot(data_max)
+plt.show()
