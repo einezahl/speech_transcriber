05559ccbadf2746984fc037128d2c9101a2c94eb
eve-contract-person <tom.reclik@gmail.com>
2022-03-31 23:06:57 +0200

    Added audio player class

---
+++ src/audio/audio_player.py
@@ -0,0 +1,53 @@
+import time
+import hydra
+from hydra.core.config_store import ConfigStore
+from hydra.utils import get_original_cwd
+import os
+import pyaudio
+import wave
+
+from src.config import RecordingConfig
+
+cs = ConfigStore.instance()
+cs.store(name="recording_config", node=RecordingConfig)
+
+class AudioPlayer:
+	def __init__(self, conf: RecordingConfig):
+		self.audio = pyaudio.PyAudio()
+		self.chunk = conf.rec_params.chunk
+
+	def start_playing(self, filename: str):
+		self.wf = wave.open(filename, 'rb')
+		self.stream = self.audio.open(
+			format=self.audio.get_format_from_width(self.wf.getsampwidth()),
+			channels=self.wf.getnchannels(),
+			rate=self.wf.getframerate(),
+			output=True,
+			stream_callback=self.callback
+		)
+		print("* playing")
+		data = self.wf.readframes(self.chunk)
+		self.stream.start_stream()
+
+	def stop_playing(self):
+		self.stream.stop_stream()
+
+	def callback(self, in_data, frame_count, time_info, status):
+		data = self.wf.readframes(self.chunk)
+		return (data, pyaudio.paContinue)
+
+	def terminate(self):
+		while self.stream.is_active():
+			time.sleep(0.1)
+		self.audio.terminate()
+
+
+@hydra.main(config_path="../conf/", config_name="conf")
+def main(conf: RecordingConfig):
+	record = AudioPlayer(conf)
+	record.start_playing(os.path.join(get_original_cwd(), './recordings/1648754506.wav'))
+	record.terminate()
+
+
+if __name__ == "__main__":
+    main()
